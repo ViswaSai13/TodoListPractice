05ef0a65594cce0bea3388230aad84b6658bce9f
ViswaSai13 <ViswaSai13@users.noreply.github.com>
2020-08-03 22:24:52 +0530

    Implementing Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,65 @@
 <template>
-  <div id="app">
-    <Todos v-bind:todos="todos" v-on:delete-todo="deleteTodo"/>
-    <AddTodo v-on:add-todo="addTodo"/>
+  <div class="container">
+    <h1>To-Do List</h1>
+    <div id="app">
+      <div>
+        <TodoNew />
+        <TodoList />
+      </div>
+    </div>
+    <h6>Done Using Vuex Store<br>(Please view in Full Screen)</h6>
   </div>
 </template>
+
 <script>
-  import Todos from './components/Todos';
-  import AddTodo from './components/AddTodo';
-  export default {
-  name: 'app',
+import TodoNew from "@/components/TodoNew.vue";
+import TodoList from "@/components/TodoList.vue";
+
+export default {
   components: {
-  Todos,
-  AddTodo
-  },
-  data() {
-  return {
-  todos: [
-  {
-  id: 1,
-  title: 'Create Todo App Project',
-  completed: false
-  },
-  {
-  id: 2,
-  title: 'Run the server & clean the project',
-  completed: false
-  },
-  {
-  id: 3,
-  title: 'Create array to store data',
-  completed: false
-  },
-  {
-  i: 4,
-  title: 'Create relevant components',
-  completed: false
-  },
-  {
-  i: 5,
-  title: 'Display the todos',
-  completed: false
+    TodoNew,
+    TodoList
   }
-  ],
-  }
-  },
-  methods: {
-  addTodo(newTodoObj) {
-  this.todos = [...this.todos, newTodoObj];
-  },
-  deleteTodo(todoId) {
-  this.todos = this.todos.filter(todo => todo.id !== todoId);
-  }
-  }
-  }
+};
 </script>
+
 <style>
+@import url("https://fonts.googleapis.com/css?family=Open+Sans");
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+
+html {
+  font-family: "Open Sans", sans-serif;
+  height: 100%;
+  color: #333;
+}
+
+body {
+  background-image: url('./assets/notepad.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: contain;
+  background-position: center;  
+  display: flex;
+  height: 100%;
+  margin: 0;
+}
+
+.container {
+  width: 20rem;
+  margin-top: 8rem;
+  margin-left: 38.5%;
+  padding: 1rem;
+}
+
+h1 {
+  text-align: center;
+  margin-top: 0;
+}
+
+h6 {
+  text-align: right;
+  font-style: italic;
+  margin-top: 0;
+}
+
 </style>
